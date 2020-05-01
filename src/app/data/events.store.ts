030310da83ddbb23f5da6bface7b8e30c5e52341
Romain Nugou <rom.nugou@gmail.com>
2020-05-01 17:08:04 -0400

    Clear default data

--- src/app/data/events.store.ts
+++ src/app/data/events.store.ts
@@ -1,35 +1,12 @@
 export const events: any = [
-  {name: 'Melbourne', date: '2012-02', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-  {name: 'Great Ocean Road', date: '2012-03', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-  {name: 'Sydney', date: '2012-04', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-  {name: 'Tasmania', date: '2012-05', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-  {name: 'New Zealand', date: '2012-07', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-  {name: 'Brisbane', date: '2012-08-01', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-  {name: 'Cairns', date: '2012-08-02', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-  {name: 'Uluru', date: '2012-08-03', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-
-  {name: 'Reykjavik', date: '2014-08-05', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-  {name: 'Vik', date: '2014-08-07', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-  {name: 'Landmannalaugar', date: '2014-09-20', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-
-  {name: 'Montréal', date: '2015-01', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-  {name: 'Chicago', date: '2015-08', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test.jpg']},
-  {name: 'Lac Saint-Jean', date: '2015-09', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test2.jpg']},
-
-  {name: 'Tulum', date: '2019-05-01', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-  {name: 'Bacalar', date: '2019-05-02', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-  {name: 'Calakmul', date: '2019-05-03', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-  {name: 'Campeche', date: '2019-05-04', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-  {name: 'Chichen Itxa', date: '2019-05-05', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-  {name: 'Valladolid', date: '2019-05-06', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-
-  {name: 'New York', date: '2019-09', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-
-  {name: 'Mont-Mégantic', date: '2019-10', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-
-  {name: 'Chamonix', date: '2020-01', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-
-  {name: 'Minca', date: '2020-03-01', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-  {name: 'Parque Tayrona', date: '2020-03-02', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
-  {name: 'Cartagena', date: '2020-03-03', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg', 'test.jpg']},
+  {name: 'Just my first event', date: '2019-01', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
+  {name: 'Event 2', date: '2019-02', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
+  {name: 'Event 3', date: '2019-03', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
+  {name: 'Event 4', date: '2019-04', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
+  {name: 'Event 5', date: '2019-05', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
+  {name: 'Event 6', date: '2020-01', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
+  {name: 'Event 7', date: '2020-02', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
+  {name: 'Event 8', date: '2020-03', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
+  {name: 'Event 9', date: '2020-04', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']},
+  {name: 'Event 10', date: '2020-05', longitude: 0, latitude: 0, folder: 'test', images: ['test.jpg', 'test.jpg', 'test2.jpg', 'test.jpg']}
 ];
